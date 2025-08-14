88873929014e3dbc711468344b710670c4130012
Rohit <rohitrai0002@gmail.com>
2025-08-14 16:53:13 +0530

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema({
 //encrypting the password only when password is new or changes not everytime
 userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
