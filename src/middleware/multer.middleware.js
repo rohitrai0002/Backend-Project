ac680fe8ab9fce7566c5e42f1062b339701a20e0
Rohit <rohitrai0002@gmail.com>
2025-08-16 23:57:14 +0530

    Cloudinary error fix and postman testing

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,7 +1,7 @@
 import multer from "multer"
 
 //cb callback
-const storage = multer.diskStorage({
+const storage =  multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp")
   },
@@ -10,6 +10,4 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage })
-
-export { upload }
+export const upload = multer({ storage })
